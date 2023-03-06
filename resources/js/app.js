08412f491f31b2409ba55dc7d08faad8d59f020c
Bilal Anwar <b.anwar21.a@gmail.com>
2023-03-06 23:33:33 +0500

    all

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import router from './router';
+import app from './layout/app.vue';
+
+createApp(app).use(router).mount('#app');
---
+++ resources/js/layout/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <div>
+            <router-link :to="{name:'Home'}">Home</router-link>
+            <router-link to="/login">Login</router-link>
+            <router-link to="/register">Register</router-link>
+        </div>
+
+        <router-view></router-view>
+    </div>
+</template>
